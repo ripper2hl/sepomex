be18abd1be83f3e6c100083317f40f40d24b7d94
Jesus Perales <jesus.perales@famsa.com>
2018-04-09 00:47:47 -0500

    * Agregando testeo a colonias

--- src/main/java/com/perales/sepomex/service/ColoniaService.java
+++ src/main/java/com/perales/sepomex/service/ColoniaService.java
@@ -3,9 +3,10 @@ package com.perales.sepomex.service;
 import com.perales.sepomex.contract.ServiceGeneric;
 import com.perales.sepomex.model.*;
 import com.perales.sepomex.repository.ColoniaRepository;
-import com.perales.util.Parser;
+import com.perales.sepomex.util.Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,163 +20,162 @@ import java.util.List;
 
 @Service
 public class ColoniaService implements ServiceGeneric<Colonia, Integer> {
-
-  @Autowired
-  private ColoniaRepository coloniaRepository;
-
-  @Autowired
-  private CodigoPostalService codigoPostalService;
-
-  @Autowired
-  private MunicipioService municipioService;
-
-  @Autowired
-  private CiudadService ciudadService;
-
-  @Autowired
-  private EstadoService estadoService;
-
-  @Autowired
-  private AsentamientoTipoService asentamientoTipoService;
-
-  @Autowired
-  private ZonaTipoService zonaTipoService;
-
-  @Autowired
-  private InegiClaveCiudadService inegiClaveCiudadService;
-
-  @Autowired
-  private InegiClaveMunicipioService inegiClaveMunicipioService;
-
+    
+    private static final String FILE_NAME = "/tmp/sepomex.txt";
+    private static final int POSICIONES_MAXIMAS_SEPARADOR = 15;
+    @Autowired
+    private ColoniaRepository coloniaRepository;
+    @Autowired
+    private CodigoPostalService codigoPostalService;
+    @Autowired
+    private MunicipioService municipioService;
+    @Autowired
+    private CiudadService ciudadService;
+    @Autowired
+    private EstadoService estadoService;
+    @Autowired
+    private AsentamientoTipoService asentamientoTipoService;
+    @Autowired
+    private ZonaTipoService zonaTipoService;
+    @Autowired
+    private InegiClaveCiudadService inegiClaveCiudadService;
+    @Autowired
+    private InegiClaveMunicipioService inegiClaveMunicipioService;
     @Autowired
     private EntityManagerFactory emf;
-
-  private static final String FILE_NAME = "/tmp/sepomex.txt";
-  private static final int POSICIONES_MAXIMAS_SEPARADOR = 15;
-
-  public Colonia buscarPorId(Integer id) {
-      return coloniaRepository.getOne(id);
-  }
-
-  public List<Colonia> buscarTodos(int page, int size) {
-    return null;
-  }
-
-  public Colonia guardar(Colonia entity) {
-    return coloniaRepository.save(entity);
-  }
-
-  public Colonia actualizar(Colonia entity) {
-    return null;
-  }
-
-  public Colonia borrar(Integer id) {
-    return null;
-  }
-
-  public Boolean cargaMasiva() throws IOException {
-    Parser parser = new Parser();
-    List<String> strings = Files.readAllLines(Paths.get(FILE_NAME), Charset.forName("UTF-8"));
-    Integer contador = 0;
-    List<Colonia> colonias = new ArrayList<>(strings.size());
-    for (String s : strings) {
-      contador++;
-      List<String> list = Arrays.asList(s.split("\\|"));
-      if (list.size() == POSICIONES_MAXIMAS_SEPARADOR) {
-        Colonia colonia = parser.convertirListaColonia(list);
-        colonias.add(colonia);
-      }
+    @Autowired
+    private Parser parser;
+    
+    @Transactional(readOnly = true)
+    public Colonia buscarPorId(Integer id) {
+        return coloniaRepository.findById(id).get();
     }
-
-    EntityManager entityManager = emf.createEntityManager();
-    entityManager.getTransaction().begin();
-      System.out.println("Comenzando transaccion");
-    int contadorTransacciones = 0;
-    for(Colonia colonia : colonias){
-      revisarColonia(colonia);
-      contadorTransacciones++;
-      System.out.println(contadorTransacciones);
-      if(contadorTransacciones == 5000){
-          System.out.println("Cerrando transaccion");
-        entityManager.getTransaction().commit();
-          System.out.println("Comenzando transaccion");
+    
+    @Transactional(readOnly = true)
+    public List<Colonia> buscarTodos(int page, int size) {
+        return coloniaRepository.findAll();
+    }
+    
+    @Transactional
+    public Colonia guardar(Colonia entity) {
+        return coloniaRepository.save(entity);
+    }
+    
+    @Transactional
+    public Colonia actualizar(Colonia entity) {
+        Colonia colonia = coloniaRepository.getOne( entity.getId() );
+        colonia.setNombre(entity.getNombre());
+        return colonia;
+    }
+    
+    @Transactional
+    public Colonia borrar(Integer id) {
+        Colonia colonia = coloniaRepository.findById(id).get();
+        coloniaRepository.delete(colonia);
+        return colonia;
+    }
+    
+    public Boolean cargaMasiva() throws IOException {
+        List<String> strings = Files.readAllLines(Paths.get(FILE_NAME), Charset.forName("UTF-8"));
+        Integer contador = 0;
+        List<Colonia> colonias = new ArrayList<>(strings.size());
+        for (String s : strings) {
+            contador++;
+            List<String> list = Arrays.asList(s.split("\\|"));
+            if (list.size() == POSICIONES_MAXIMAS_SEPARADOR) {
+                Colonia colonia = parser.convertirListaColonia(list);
+                colonias.add(colonia);
+            }
+        }
+        EntityManager entityManager = emf.createEntityManager();
         entityManager.getTransaction().begin();
-      }
+        System.out.println("Comenzando transaccion");
+        int contadorTransacciones = 0;
+        for (Colonia colonia : colonias) {
+            revisarColonia(colonia);
+            contadorTransacciones++;
+            System.out.println(contadorTransacciones);
+            if (contadorTransacciones == 5000) {
+                System.out.println("Cerrando transaccion");
+                entityManager.getTransaction().commit();
+                System.out.println("Comenzando transaccion");
+                entityManager.getTransaction().begin();
+            }
+        }
+        entityManager.getTransaction().commit();
+        return true;
     }
-    entityManager.getTransaction().commit();
-    return true;
-  }
-
-  private void revisarColonia(Colonia colonia) {
-    CodigoPostal codigoPostal = codigoPostalService.findByNombre(colonia.getCodigoPostal().getNombre());
-    if(codigoPostal == null){
-      codigoPostal = codigoPostalService.guardar(colonia.getCodigoPostal());
+    
+    private void revisarColonia(Colonia colonia) {
+        CodigoPostal codigoPostal = codigoPostalService.findByNombre(colonia.getCodigoPostal().getNombre());
+        if (codigoPostal == null) {
+            codigoPostal = codigoPostalService.guardar(colonia.getCodigoPostal());
+        }
+        colonia.setCodigoPostal(codigoPostal);
+        
+        CodigoPostal codigoPostalAdministracionAsentamiento = codigoPostalService
+                .findByNombre(colonia.getCodigoPostalAdministracionAsentamiento().getNombre());
+        if (codigoPostalAdministracionAsentamiento == null) {
+            codigoPostalAdministracionAsentamiento = codigoPostalService.guardar(colonia.getCodigoPostalAdministracionAsentamiento());
+        }
+        colonia.setCodigoPostalAdministracionAsentamiento(codigoPostalAdministracionAsentamiento);
+        
+        CodigoPostal codigoPostalAdministracionAsentamientoOficina = codigoPostalService
+                .findByNombre(colonia.getCodigoPostalAdministracionAsentamientoOficina().getNombre());
+        if (codigoPostalAdministracionAsentamientoOficina == null) {
+            codigoPostalAdministracionAsentamientoOficina = codigoPostalService.guardar(colonia.getCodigoPostalAdministracionAsentamientoOficina());
+        }
+        colonia.setCodigoPostalAdministracionAsentamientoOficina(codigoPostalAdministracionAsentamientoOficina);
+        
+        InegiClaveCiudad inegiClaveCiudad = inegiClaveCiudadService.findFirstByNombre(colonia.getInegiClaveCiudad().getNombre());
+        if (inegiClaveCiudad == null) {
+            inegiClaveCiudad = inegiClaveCiudadService.guardar(colonia.getInegiClaveCiudad());
+        }
+        colonia.setInegiClaveCiudad(inegiClaveCiudad);
+        
+        InegiClaveMunicipio inegiClaveMunicipio = inegiClaveMunicipioService.findFirstByNombre(colonia.getInegiClaveMunicipio().getNombre());
+        if (inegiClaveMunicipio == null) {
+            inegiClaveMunicipio = inegiClaveMunicipioService.guardar(colonia.getInegiClaveMunicipio());
+        }
+        colonia.setInegiClaveMunicipio(inegiClaveMunicipio);
+        
+        AsentamientoTipo asentamientoTipo = asentamientoTipoService
+                .findBySepomexClave(colonia.getAsentamientoTipo().getSepomexClave());
+        if (asentamientoTipo == null) {
+            asentamientoTipo = asentamientoTipoService.guardar(colonia.getAsentamientoTipo());
+        }
+        colonia.setAsentamientoTipo(asentamientoTipo);
+        
+        Estado estado = estadoService.findByInegiClave(colonia.getEstado().getInegiClave());
+        if (estado == null) {
+            estado = estadoService.guardar(colonia.getEstado());
+            colonia.setEstado(estado);
+            System.out.println(estado);
+        }
+        colonia.setEstado(estado);
+        
+        Ciudad ciudad = ciudadService.findFirstByNombreAndEstadoId(colonia.getCiudad().getNombre(), estado.getId());
+        if (ciudad == null) {
+            colonia.getCiudad().setEstado(estado);
+            ciudad = ciudadService.guardar(colonia.getCiudad());
+        }
+        colonia.setCiudad(ciudad);
+        
+        Municipio municipio = municipioService.findFirstByNombreAndEstadoId(colonia.getMunicipio().getNombre(), estado.getId());
+        if (municipio == null) {
+            colonia.getMunicipio().setEstado(estado);
+            municipio = municipioService.guardar(colonia.getMunicipio());
+        }
+        colonia.setMunicipio(municipio);
+        
+        ZonaTipo zonaTipo = zonaTipoService.findByNombre(colonia.getZonaTipo().getNombre());
+        if (zonaTipo == null) {
+            zonaTipo = zonaTipoService.guardar(colonia.getZonaTipo());
+            colonia.setZonaTipo(zonaTipo);
+        }
+        colonia.setZonaTipo(zonaTipo);
+        
+        guardar(colonia);
     }
-    colonia.setCodigoPostal(codigoPostal);
-
-    CodigoPostal codigoPostalAdministracionAsentamiento = codigoPostalService
-            .findByNombre(colonia.getCodigoPostalAdministracionAsentamiento().getNombre());
-    if(codigoPostalAdministracionAsentamiento == null){
-      codigoPostalAdministracionAsentamiento = codigoPostalService.guardar(colonia.getCodigoPostalAdministracionAsentamiento());
-    }
-    colonia.setCodigoPostalAdministracionAsentamiento(codigoPostalAdministracionAsentamiento);
-
-    CodigoPostal codigoPostalAdministracionAsentamientoOficina = codigoPostalService
-            .findByNombre(colonia.getCodigoPostalAdministracionAsentamientoOficina().getNombre());
-    if(codigoPostalAdministracionAsentamientoOficina == null){
-      codigoPostalAdministracionAsentamientoOficina = codigoPostalService.guardar(colonia.getCodigoPostalAdministracionAsentamientoOficina());
-    }
-    colonia.setCodigoPostalAdministracionAsentamientoOficina(codigoPostalAdministracionAsentamientoOficina);
-
-    InegiClaveCiudad inegiClaveCiudad = inegiClaveCiudadService.findFirstByNombre( colonia.getInegiClaveCiudad().getNombre() );
-    if(inegiClaveCiudad == null){
-        inegiClaveCiudad = inegiClaveCiudadService.guardar(colonia.getInegiClaveCiudad());
-    }
-    colonia.setInegiClaveCiudad(inegiClaveCiudad);
-
-    InegiClaveMunicipio inegiClaveMunicipio = inegiClaveMunicipioService.findFirstByNombre( colonia.getInegiClaveMunicipio().getNombre() );
-    if(inegiClaveMunicipio == null){
-      inegiClaveMunicipio = inegiClaveMunicipioService.guardar(colonia.getInegiClaveMunicipio() );
-    }
-    colonia.setInegiClaveMunicipio(inegiClaveMunicipio);
-
-    AsentamientoTipo asentamientoTipo = asentamientoTipoService
-        .findBySepomexClave(colonia.getAsentamientoTipo().getSepomexClave());
-    if (asentamientoTipo == null) {
-      asentamientoTipo = asentamientoTipoService.guardar(colonia.getAsentamientoTipo());
-    }
-    colonia.setAsentamientoTipo(asentamientoTipo);
-
-    Estado estado = estadoService.findByInegiClave(colonia.getEstado().getInegiClave());
-    if (estado == null) {
-      estado = estadoService.guardar(colonia.getEstado());
-      colonia.setEstado(estado);
-      System.out.println(estado);
-    }
-    colonia.setEstado(estado);
-
-    Ciudad ciudad = ciudadService.findFirstByNombreAndEstadoId(colonia.getCiudad().getNombre(), estado.getId());
-    if (ciudad == null) {
-      colonia.getCiudad().setEstado(estado);
-      ciudad = ciudadService.guardar(colonia.getCiudad());
-    }
-    colonia.setCiudad(ciudad);
-
-    Municipio municipio = municipioService.findFirstByNombreAndEstadoId(colonia.getMunicipio().getNombre(), estado.getId());
-    if (municipio == null) {
-      colonia.getMunicipio().setEstado(estado);
-      municipio = municipioService.guardar( colonia.getMunicipio() );
-    }
-    colonia.setMunicipio(municipio);
-
-    ZonaTipo zonaTipo = zonaTipoService.findByNombre(colonia.getZonaTipo().getNombre());
-    if (zonaTipo == null) {
-      zonaTipo = zonaTipoService.guardar(colonia.getZonaTipo());
-      colonia.setZonaTipo(zonaTipo);
-    }
-    colonia.setZonaTipo(zonaTipo);
-
-    guardar(colonia);
-  }
-
+    
 }
--- src/test/java/com/perales/sepomex/service/ColoniaServiceTest.java
+++ src/test/java/com/perales/sepomex/service/ColoniaServiceTest.java
@@ -3,7 +3,9 @@ package com.perales.sepomex.service;
 import com.perales.sepomex.configuration.AppTestConfig;
 import com.perales.sepomex.model.Colonia;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,8 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
@@ -22,6 +28,9 @@ import static org.hamcrest.core.Is.is;
 public class ColoniaServiceTest {
 
     private MockMvc mockMvc;
+    
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -52,14 +61,32 @@ public class ColoniaServiceTest {
 
     @Test
     public void guardar() {
+        Colonia colonia = new Colonia();
+        colonia.setNombre("Cañada blanca");
+        Colonia coloniaGuardada = coloniaService.guardar(colonia);
+        assertThat("Deberia tener un id", coloniaGuardada.getId(), is( notNullValue() ) );
     }
 
     @Test
     public void actualizar() {
+        String nombreColonia = "cañada blanca";
+        Colonia colonia = new Colonia();
+        colonia.setNombre( "canada blanca" );
+        colonia = coloniaService.guardar( colonia );
+        colonia.setNombre( nombreColonia );
+        coloniaService.actualizar( colonia );
+        Colonia coloniaEncontrada = coloniaService.buscarPorId( colonia.getId() );
+        assertThat("Deberia tener el nombre corregido", nombreColonia, is( equalTo( coloniaEncontrada.getNombre() ) ) );
     }
-
+    
     @Test
     public void borrar() {
+        Colonia colonia = new Colonia();
+        colonia.setNombre("Cañada blanca");
+        Colonia coloniaGuardada = coloniaService.guardar(colonia);
+        coloniaService.borrar( coloniaGuardada.getId() );
+        exception.expect(NoSuchElementException.class);
+        Colonia coloniaEncontrada = coloniaService.buscarPorId(coloniaGuardada.getId());
     }
 
 }
