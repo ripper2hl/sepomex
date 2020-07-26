c23d6d7630adb4db9f820b93a7a576df99931bf7
Israel Perales <israel-perales@outlook.com>
2020-07-26 01:21:23 -0500

    * Agregando configuracion para trabajar en local. * Actualizando algunas dependencias.

---
+++ src/main/java/com/perales/sepomex/configuration/AppConfigLocal.java
@@ -0,0 +1,122 @@
+package com.perales.sepomex.configuration;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableJpaRepositories( basePackages = { "com.perales.sepomex.repository" })
+@ComponentScan("com.perales.sepomex")
+@Profile({ "local" })
+@Log4j2
+public class AppConfigLocal implements WebMvcConfigurer {
+
+    private LocalContainerEntityManagerFactoryBean emf;
+    private HibernateJpaVendorAdapter hibernateJpaVendorAdapter;
+    private DataSource dataSource;
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass( org.postgresql.Driver.class);
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/sepomex");
+        dataSource.setUsername("sepomex");
+        dataSource.setPassword("sepomex");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("com.perales.sepomex.model");
+        emf.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        emf.setJpaProperties(hibernateProperties());
+        return emf;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql( false );
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.POSTGRESQL);
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(emf);
+        return jpaTransactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.jdbc.lob.non_contextual_creation", "true");
+        properties.put("hibernate.hbm2ddl.auto", "none");
+        properties.put("hibernate.jdbc.batch_size", "25");
+        properties.put("hibernate.order_inserts", "true");
+        properties.put("hibernate.order_updates", "true");
+        properties.put("hibernate.search.default.directory_provider", "filesystem");
+        properties.put("hibernate.search.default.indexBase", "/tmp/sepomex-indices/");
+        return properties;
+    }
+    
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        MethodValidationPostProcessor mvProcessor = new MethodValidationPostProcessor();
+        mvProcessor.setValidator(validator());
+        return mvProcessor;
+    }
+    
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setProviderClass(HibernateValidator.class);
+        validator.afterPropertiesSet();
+        return validator;
+    }
+    
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
