1fc542d139a127dde0840f806c9a476f2b9420d9
Israel Perales <israel-perales@outlook.com>
2021-04-03 00:41:39 -0600

    * Ugly workaround

--- src/main/java/com/perales/sepomex/functions/ObtenerEstados.java
+++ src/main/java/com/perales/sepomex/functions/ObtenerEstados.java
@@ -1,7 +1,10 @@
 package com.perales.sepomex.functions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.perales.sepomex.model.Estado;
 import com.perales.sepomex.service.EstadoService;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,13 +12,18 @@ import org.springframework.data.domain.Page;
 import java.util.function.Function;
 
 @Log4j2
-public class ObtenerEstados implements Function<Void, Page<Estado>> {
+public class ObtenerEstados implements Function<Void, String> {
     
     @Autowired
     private EstadoService estadoService;
     
+    @SneakyThrows
     @Override
-    public Page<Estado> apply(Void unused) {
-        return estadoService.buscarTodos(0,33);
+    public String apply(Void unused) {
+        Page<Estado> page = estadoService.buscarTodos(0, 33);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Hibernate5Module());
+        String objectAsString = objectMapper.writeValueAsString(page);
+        return objectAsString;
     }
 }
