a56b7b53077243c759e440af6fb82c2ad7bb81b7
Israel Perales <jesus.perales@banregio.com>
2021-08-08 01:36:28 -0500

    * Agregando funcion de busqueda de municipio por estado

---
+++ src/main/java/com/perales/sepomex/util/FunctionMunicipiosPorEstadoPojo.java
@@ -0,0 +1,10 @@
+package com.perales.sepomex.util;
+
+import lombok.Data;
+
+@Data
+public class FunctionMunicipiosPorEstadoPojo {
+    private Integer estadoId;
+    private Integer page;
+    private Integer size;
+}
